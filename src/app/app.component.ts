bdba81209f6fffcb8aee82e15b56849604d65484
Afaren <550047450@qq.com>
2017-07-13 18:32:35 +0800

    display pirate list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,19 @@ export class Pirate {
  }
 
 
+const PIRATES:  Pirate[] = [
+  { id: 1, 	name: 'Monkey D. Luffy' },
+  { id: 2, 	name: 'Roronoa Zoro' },
+  { id: 3, 	name: 'Nami' },
+  { id: 4, 	name: 'Usopp'},
+  { id: 5, 	name: 'Vinsmoke Sanji'},
+  { id: 6, 	name: 'Tony Tony Chopper'},
+  { id: 7, 	name: 'Nico Robin'},
+  { id: 8, 	name: 'Franky'},
+  { id: 9, 	name: 'Brook'}
+]
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,6 +26,7 @@ export class Pirate {
 })
 
 export class AppComponent {
+  pirates = PIRATES;
   title = 'Straw Hat Pirates';
   pirate: Pirate = {
     id: 1,
