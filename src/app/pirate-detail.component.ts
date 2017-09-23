189e85f97b21979a2409bd6bb00d54d96ec53b6c
Afaren <550047450@qq.com>
2017-09-23 20:31:13 +0800

    property binding expression

--- src/app/pirate-detail.component.ts
+++ src/app/pirate-detail.component.ts
@@ -9,5 +9,5 @@ import { Pirate } from './pirate';
 
 
 export class PirateDetailComponent {
-  @Input() pirate: Pirate;
+  @Input() pirate: Pirate; // 我们要绑定到的目标属性必须是一个输入属性，否则Angular会拒绝绑定，并抛出一个错误。
 }
